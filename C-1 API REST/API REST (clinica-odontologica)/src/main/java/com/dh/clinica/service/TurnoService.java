707d0b787a92f37526d0b025da435bb791ba4675
Laura Peralta <78451273+laurapralta@users.noreply.github.com>
2022-06-17 16:23:19 -0500

    Add files via upload

---
+++ C-1 API REST/API REST (clinica-odontologica)/src/main/java/com/dh/clinica/service/TurnoService.java
@@ -0,0 +1,37 @@
+package com.dh.clinica.service;
+
+import com.dh.clinica.model.Odontologo;
+import com.dh.clinica.repository.IDao;
+import com.dh.clinica.model.Turno;
+
+import java.util.List;
+
+public class TurnoService {
+
+    private IDao<Turno> turnoRepository;
+
+    public TurnoService(IDao<Turno> turnoRepository) {
+        this.turnoRepository = turnoRepository;
+    }
+
+    public Turno registrarTurno(Turno turno){
+        return  turnoRepository.guardar(turno);
+    }
+    public List<Turno> listar(){
+        return turnoRepository.buscarTodos();
+    }
+
+    public Turno buscar(Integer id) {
+        return turnoRepository.buscar(id);
+    }
+
+    public Turno actualizar(Turno turno) {
+        return turnoRepository.actualizar(turno);
+    }
+
+    public void eliminar(Integer id) {
+        turnoRepository.eliminar(id);
+    }
+
+
+}
